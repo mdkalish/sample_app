50a48373a964e7ddc0f3d75fa13ba97b613eb526
mdkalish <mdkalish4git@gmail.com>
2014-10-08 00:09:47 +0200

    Finish log in/log out

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class SessionsHelperTest < ActionView::TestCase
+
+    # 1. Define a user variable using the fixtures.
+    # 2. Call the remember method to remember the given user.
+    # 3. Verify that current_user is equal to the given user. 
+    
+  test "current_user" do 
+    user = users(:michael)
+    remember(user)
+    assert_equal user, current_user
+  end
 end
